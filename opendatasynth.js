2360ad2b5b32c7c1c910171d53247aa78d43b0b9
Aquassaut <jeremy.autran@opendatasoft.com>
2017-03-24 18:18:37 +0100

    Widgetized app

--- opendatasynth.js
+++ opendatasynth.js
@@ -1,114 +1,115 @@
-var source, wavesurfer;
-var domain = "https://data.opendatasoft.com";
-var context = new AudioContext();
+angular.module('ods-widgets').controller('ExampleController', ['$scope', function($scope) {
+    var source;
+    $scope.wavesurfer = null;
 
-var suggestedDatasets = function(req, res) {
-    var search = req.term;
-    $.get(domain + "/api/datasets/1.0/search/?sort=records_count&refine.features=analyze&rows=10&q=" + search, function(result) {
-        data = result.datasets.map(function(dataset) {
-           return {
-               value: dataset.datasetid,
-               label: dataset.metas.title + "(" + dataset.metas.source_domain_title + ")"
-           };
-        });
-        return res(data);    
-    });    
-};
-$("#dataset").autocomplete({
-    source: suggestedDatasets
-});
+    var context = new AudioContext();
 
-$("#dataset").change(function(){
-    var fieldSelect = $('#field');
-    fieldSelect.empty();
-    var datasetId = $("#dataset")[0].value;
-    $.get(domain + "/api/datasets/1.0/" + datasetId + "/", function(result) {
-        var numericFields = result.fields.filter(function(field) {
-            return field.type === "double" || field.type === "int";
-        });
-        if (numericFields.length === 0) {
-            return fieldSelect.append(new Option('No suitable field :(', 'null'));
+    $scope.domUrl = null;
+    $scope.local = {
+        dts: null,
+        fld: null,
+        srt: null
+    };
+
+    $scope.fieldSortable = function(item) {
+        if (item.type === "int" || item.type === "double" || item.type === "date" || item.type === "datetime") {
+            return true;
         }
-        numericFields.map(function(numericField) {
-            fieldSelect.append(new Option(numericField.label, numericField.name));
-        });
-    });
-});
-$("#dataset").change();
-
-var play = function() {
-    if (source) {
-      source.start();
-    }
-    $("#go")[0].hidden = false;
-    $("#play")[0].hidden = true;
-};
-
-var go = function() {
-    if (wavesurfer) {
-        wavesurfer.destroy();
-        $("#play")[0].hidden = false;
-    }
-    var datasetId = $("#dataset")[0].value;
-    var fieldToListenTo = $("#field")[0].value;
-  
-    $.get(domain + "/api/records/1.0/search/?rows=10000&dataset=" +  datasetId, function(result) {
-        var records = result.records;
-        var numOfRecords = records.length;
-        // Find min and max val, so we can normalize values later.
-        // Use the loop to create a tidier array that we can feed to smooth
-        var minVal = null;
-        var maxVal = null;
-        var recVals = [];
-        for (var i = 0; i < numOfRecords; i += 1) {
-            var fval = records[i].fields[fieldToListenTo];
-            if (typeof fval === "number" && fval === fval) {
-                // avoid type errors
-                if (minVal === null || fval < minVal) {
-                    minVal = fval;
-                }
-                if (maxVal === null || fval > maxVal) {
-                    maxVal = fval;
-                }
-                recVals.push(fval);
+        if (! item.annotations) {
+            return false;
+        }
+        for (var i = 0 ; i < item.annotations.length; i += 1) {
+            if (item.annotations[i].name === "sortable") {
+                return true;
             }
         }
+        return false;
+    };
 
-        console.debug("found min", minVal, "and max", maxVal);
+    $scope.fieldNumeric = function(item) {
+        return item.type === "int" || item.type === "double";
+    };
 
-        // Normalize the list of values in the interval [-1,1] in order to create
-        // a PCM audio waveform
-        recVals = recVals.map(function(val) {
-            return 2 * ((val - minVal) / (maxVal - minVal)) - 1;
-        });
+    $scope.play = function() {
+        if ($scope.wavesurfer) {
+            $scope.wavesurfer.play();
+        }
+    };
 
-        // Add 0 value at beginning and end to create a fade in/out, avoiding pops
-        recVals.unshift(0);
-        recVals.push(0);
-
-        // Create a smooth function in order to interpollate extra points in interval
-        var smooth = Smooth(recVals);
-
-        //set up audio
-        var duration = 2; //2 second of audio
-        var sampleRate = 44100; //Works in firefox and chrome. Not pretty though
-        var numOfSamples = duration * sampleRate;
-
-        var audioBuffer = context.createBuffer(1, numOfSamples, sampleRate);
-        var dataBuffer = audioBuffer.getChannelData(0);
-        // Loop on future sample indices, compute sample value and stick it in audio buffer
-        for (var j = 0; j < numOfSamples ; j += 1) {
-            //if ( j % Math.floor(numOfSamples / 10) === 0) console.log((100 * j / numOfSamples) + "%");
-            var nval = smooth(j * recVals.length / numOfSamples);
-            dataBuffer[j] = nval;
+    $scope.$watch('local', function(nv, ov) {
+        if (ov.dts !== nv.dts) {
+            $scope.local.fld = null;
+            $scope.local.srt = null;
+            return;
         }
 
-        wavesurfer = WaveSurfer.create({
-            container: '#waveform',
-            waveColor: 'violet',
-            progressColor: 'purple'
+        if (! ($scope.local.dts && $scope.local.fld) ) {
+            return;
+        }
+
+        console.log($scope.local);
+
+        if ($scope.wavesurfer) {
+            $scope.wavesurfer.destroy();
+        }
+        var url = $scope.$$childHead.catctx.domainUrl + "/api/records/1.0/search/?rows=10000&dataset=" + $scope.local.dts;
+        if ($scope.local.srt) {
+            url += "&sort=" + $scope.local.srt;
+        }
+        $.get(url, function(result) {
+            var records = result.records;
+            var numOfRecords = records.length;
+            // Find min and max val, so we can normalize values later.
+            // Use the loop to create a tidier array that we can feed to smooth
+            var minVal = null;
+            var maxVal = null;
+            var recVals = [];
+            for (var i = 0; i < numOfRecords; i += 1) {
+                var fval = records[i].fields[$scope.local.fld];
+                if (typeof fval === "number" && fval === fval) {
+                    // avoid type errors
+                    if (minVal === null || fval < minVal) {
+                        minVal = fval;
+                    }
+                    if (maxVal === null || fval > maxVal) {
+                        maxVal = fval;
+                    }
+                    recVals.push(fval);
+                }
+            }
+            // Normalize the list of values in the interval [-1,1] in order to create
+            // a PCM audio waveform
+            recVals = recVals.map(function(val) {
+                return 2 * ((val - minVal) / (maxVal - minVal)) - 1;
+            });
+
+            // Add 0 value at beginning and end to create a fade in/out, avoiding pops
+            recVals.unshift(0);
+            recVals.push(0);
+
+            // Create a smooth function in order to interpollate extra points in interval
+            var smooth = Smooth(recVals);
+
+            //set up audio
+            var duration = 2; //2 second of audio
+            var sampleRate = 44100; //Works in firefox and chrome. Not pretty though
+            var numOfSamples = duration * sampleRate;
+
+            var audioBuffer = context.createBuffer(1, numOfSamples, sampleRate);
+            var dataBuffer = audioBuffer.getChannelData(0);
+            // Loop on future sample indices, compute sample value and stick it in audio buffer
+            for (var j = 0; j < numOfSamples ; j += 1) {
+                //if ( j % Math.floor(numOfSamples / 10) === 0) console.log((100 * j / numOfSamples) + "%");
+                var nval = smooth(j * recVals.length / numOfSamples);
+                dataBuffer[j] = nval;
+            }
+
+            $scope.wavesurfer = WaveSurfer.create({
+                container: '#waveform',
+                waveColor: 'violet',
+                progressColor: 'purple'
+            });
+            $scope.wavesurfer.loadDecodedBuffer(audioBuffer);
         });
-        wavesurfer.loadDecodedBuffer(audioBuffer);
-        $("#play")[0].hidden = false;     
-    });
-};
+    }, true);
+}]);
