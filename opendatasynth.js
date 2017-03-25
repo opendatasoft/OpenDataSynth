bbfd0a9bce46377080352d8e5c418a5316614e9d
Aquassaut <jeremy.autran@gmail.com>
2017-03-25 19:55:28 +0100

    Add a spinner when working on next audio

--- opendatasynth.js
+++ opendatasynth.js
@@ -1,6 +1,21 @@
-angular.module('ods-widgets').controller('ExampleController', ['$scope', function($scope) {
+angular.module('ods-widgets').controller('OpenDataSynthController', ['$scope', '$http', function($scope, $http) {
     var source;
+    var duration = 2; //2 second of audio
+    var sampleRate = 44100; //Works in most browsers
+    var numOfSamples = duration * sampleRate;
     var context = new AudioContext();
+    var audioBuffer = context.createBuffer(1, numOfSamples, sampleRate);
+    var dataBuffer = audioBuffer.getChannelData(0);
+
+    $scope.working = false;
+    $scope.domUrl = null;
+    $scope.dts = {
+        id: null
+    };
+    $scope.local = {
+        fld: null,
+        srt: null
+    };
     $scope.wavesurfer = WaveSurfer.create({
         container: '#waveform',
         fillParent: true,
@@ -8,25 +23,6 @@ angular.module('ods-widgets').controller('ExampleController', ['$scope', functio
         waveColor: 'darkblue',
         progressColor: 'purple'
     });
-
-    (function initWaveSurfer() {
-        var duration = 2;
-        var sampleRate = 44100;
-        var numOfSamples = duration * sampleRate;
-        var audioBuffer = context.createBuffer(1, numOfSamples, sampleRate);
-        var dataBuffer = audioBuffer.getChannelData(0);
-        for (var i = 0; i < numOfSamples ; i += 1) {
-            dataBuffer[i] = 0;
-        }
-        $scope.wavesurfer.loadDecodedBuffer(audioBuffer);
-    })();
-
-    $scope.domUrl = null;
-    $scope.local = {
-        dts: null,
-        fld: null,
-        srt: null
-    };
 
     $scope.fieldSortable = function(item) {
         if (item.type === "int" || item.type === "double" || item.type === "date" || item.type === "datetime") {
@@ -48,16 +44,17 @@ angular.module('ods-widgets').controller('ExampleController', ['$scope', functio
     };
 
     $scope.$watch('local', function(nv, ov) {
-        if (! ($scope.local.dts && $scope.local.fld) ) {
+        if (! ($scope.dts.id && $scope.local.fld) ) {
+            // The user is still working on the selection
             return;
         }
-
-        var url = $scope.$$childHead.catctx.domainUrl + "/api/records/1.0/search/?rows=10000&dataset=" + $scope.local.dts;
+        $scope.working = true;
+        var url = $scope.$$childHead.catctx.domainUrl + "/api/records/1.0/search/?rows=10000&dataset=" + $scope.dts.id;
         if ($scope.local.srt) {
             url += "&sort=" + $scope.local.srt;
         }
-        $.get(url, function(result) {
-            var records = result.records;
+        $http.get(url).then(function(result) {
+            var records = result.data.records;
             var numOfRecords = records.length;
             // Find min and max val, so we can normalize values later.
             // Use the loop to create a tidier array that we can feed to smooth
@@ -87,30 +84,29 @@ angular.module('ods-widgets').controller('ExampleController', ['$scope', functio
                 // let's not divide by 0. Instead since that thing is a flat line, let's make it flat for real.
                 recVals = [0];
             }
-
             // Add 0 value at beginning and end to create a fade in/out, avoiding pops
             recVals.unshift(0);
             recVals.push(0);
-
             // Create a smooth function in order to interpollate extra points in interval
             var smooth = Smooth(recVals);
-
-            //set up audio
-            var duration = 2; //2 second of audio
-            var sampleRate = 44100; //Works in firefox and chrome. Not pretty though
-            var numOfSamples = duration * sampleRate;
-
-            var audioBuffer = context.createBuffer(1, numOfSamples, sampleRate);
-            var dataBuffer = audioBuffer.getChannelData(0);
             // Loop on future sample indices, compute sample value and stick it in audio buffer
             for (var j = 0; j < numOfSamples ; j += 1) {
-                //if ( j % Math.floor(numOfSamples / 10) === 0) console.log((100 * j / numOfSamples) + "%");
                 var nval = smooth(j * recVals.length / numOfSamples);
                 dataBuffer[j] = nval;
             }
-
             $scope.wavesurfer.empty();
             $scope.wavesurfer.loadDecodedBuffer(audioBuffer);
+            $scope.working = false;
+            //$scope.$apply()
         });
     }, true);
+
+    (function initWaveSurfer() {
+        $scope.working = true;
+        for (var i = 0; i < numOfSamples ; i += 1) {
+            dataBuffer[i] = 0;
+        }
+        $scope.working = false;
+        $scope.wavesurfer.loadDecodedBuffer(audioBuffer);
+    })();
 }]);
